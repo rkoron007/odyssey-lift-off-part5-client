a2409078a3352cbaf522eb8a0abd819d8b3cdd41
Rose Koron <rkoron007@gmail.com>
2022-01-07 14:48:51 -0800

    Add Heroku URI to Client

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GlobalStyles from './styles'
+import Pages from './pages'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://odyssey-liftoff-part5.herokuapp.com/',
   cache: new InMemoryCache(),
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
